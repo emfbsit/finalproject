bdd86035b096fe4bd3219a2fa1d616e802319831
fordanedward <emfbsit@gmail.com>
2024-12-07 08:04:44 +0800

    main.go

---
+++ backend/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", index)
+	app.Get("/api/quizzes", getQuizzes)
+
+	log.Fatal(app.Listen(":3000"))
+}
+
+func index(c *fiber.Ctx) error {
+	return c.SendString("Setting up")
+}
+
+func getQuizzes(c *fiber.Ctx) error {
+	list := []map[string]any{
+		{
+			"test": 123,
+		},
+	}
+	return c.JSON(list)
+}
